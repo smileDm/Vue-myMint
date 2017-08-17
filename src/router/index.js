07b756caf1b6a731a79d2b02929398ebbb221c9b
dingMiao <smiledm@163.com>
2017-08-17 21:36:23 +0800

    更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Hello from '../components/Hello'
 import Zhuye from '../views/Zhuye/Zhuye'
 import Personal from '../views/Personal/Personal'
 //import Login from './../views/Login'
@@ -8,6 +9,11 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
+  	{
+      path: '/',redirect: '/Zhuye', //重定向
+      name: 'Hello',
+      component: Hello
+    },
     {
       path: '/Zhuye', name: 'Zhuye', component: Zhuye
       //path: '/', name: 'Login', component: Login
